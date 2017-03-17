7d1e3752aac99200f926c2211131da915385f558
thanksdanny <hj-07@163.com>
2017-03-18 01:21:19 +0800

    init

---
+++ Podfile
@@ -0,0 +1,12 @@
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '7.0'
+
+target 'Testerhome' do
+	pod 'MJRefresh', '~> 3.1.0'
+	pod 'Masonry', '~> 0.6.3'
+	pod 'MMDrawerController', '~> 0.5.7'
+
+
+end
---
+++ Testerhome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Testerhome
+//
+//  Created by Danny Ho on 18/03/2017.
+//  Copyright © 2017 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
